9423d64b5465a68c95df5aad7efe909d6086fbaf
mdsameer2002 <smoham64@CougarNet.UH.EDU>
2026-05-31 16:40:20 -0400

    hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @GetMapping("/")
     public String hello()
     {
-        return "Hello from CI/CD Pipeline.....";
+        return "Hello from CI/CD Pipeline check";
     }
 
 }
